d6636bba13ebf667ac3ea02d8c64a9feb83b690a
cbird808 <chris.bird@tamucc.edu>
2022-03-06 08:18:53 -0600

    Update pgdVCF2ARLEQUIN.bash

--- pgdVCF2ARLEQUIN.bash
+++ pgdVCF2ARLEQUIN.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# ex
+# bash pgdVCF2ARLEQUIN.bash genepop_infile.txt
+
+# edit this path for your computer
 pgdJAR=~/Downloads/PGDSpider_2.1.1.5/PGDSpider2-cli.jar
-inFILE=~/Projects/PuntioplitesFalcifer/fltrVCF_4revision/Pfalcifer.3.7.Fltr20.2.randSNPperLoc.neutral.recode.vcf
-inFORMAT=VCF
+
+# you shouldn't have to edit anything from here forward
+inFILE=$1inFORMAT=VCF
 outFileEXT=arp
 outFORMAT=ARLEQUIN
 spidFILE=~/Projects/PuntioplitesFalcifer/pgdspider/vcf2arlequin.spid
